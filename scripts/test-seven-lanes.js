58782eb0eb22686f6c01dd5f2ea0c09d36c4aec0
Nirmal <44404757+Nirmal2000@users.noreply.github.com>
2026-08-25 11:15:57 +0000

    seo: match footer social icons to the original landing 1em size
    Iconify defaulted to 1em in the text-sm footer. Keep the SVGs at that size on landing and blog.

--- scripts/test-seven-lanes.js
+++ scripts/test-seven-lanes.js
@@ -154,6 +154,8 @@ for (const rel of ['index.html', 'about/index.html']) {
     }
     if ((footer.match(/<svg[\s\S]*?<\/svg>/g) || []).length >= 5) ok.push(`footer icons ${rel}`);
     else fail.push(`footer missing svg icons ${rel}`);
+    if (footer.includes('width="1em"')) ok.push(`footer icon size 1em ${rel}`);
+    else fail.push(`footer icons not 1em ${rel}`);
 }
 
 console.log(ok.map((s) => `OK  ${s}`).join('\n'));
